e5e130c3ba37efdc383de034f3efbc3871ad39d8
c3sare <marcinm222@gmail.com>
2022-06-25 18:58:32 +0200

    improved alongAgo function

--- src/routes/Index.tsx
+++ src/routes/Index.tsx
@@ -1,6 +1,6 @@
 import "../styles/Posts.css";
 
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 interface Post {
   id: number;
@@ -32,7 +32,8 @@ const posts: Post[] = [
     userId: 1,
     userName: "Anonimowy",
     title: "tytuł",
-    addDate: "Fri Jun 24 2022 15:12:00 GMT+0200 (czas środkowoeuropejski letni)",
+    addDate:
+      "Fri Jun 24 2022 15:12:00 GMT+0200 (czas środkowoeuropejski letni)",
     img: "/images/upload/1.jpg",
     likes: 0,
     comments: [
@@ -57,30 +58,50 @@ const posts: Post[] = [
 ];
 
 const Index = () => {
-
   const alongAgo = (time: string) => {
     const currentTime = new Date().getTime();
     const addTime = new Date(time).getTime();
 
-    const inSeconds = (currentTime-addTime)/1000;
+    const inSeconds = (currentTime - addTime) / 1000;
 
-    if(inSeconds < 60) {
-      return `Dodane ${Math.floor(inSeconds)} sekund temu`;
-    } else if(inSeconds >= 60 && inSeconds < 60*60) {
-      return `Dodane ${Math.floor(inSeconds/60)} minut temu`;
-    } else if(inSeconds >= 60*60 && inSeconds < 60*60*24) {
-      return `Dodane ${Math.floor(inSeconds/60/60)} godzin temu`;
-    } else if(inSeconds >= 60*60*24 && inSeconds < 60*60*24*7) {
-      return `Dodane ${Math.floor(inSeconds/60/60/24)} dni temu`;
-    } else if(inSeconds >= 60*60*24*7 && inSeconds < 60*60*24*30) {
-      return `Dodane ${Math.floor(inSeconds/60/60/24/7)} tygodnie temu`;
-    } else if(inSeconds >= 60*60*24*30 && inSeconds < 60*60*24*365) {
-      return `Dodane ${Math.floor(inSeconds/60/60/24/30)} miesięcy temu`;
-    } else if(inSeconds >= 60*60*24*365) {
-      return `Dodane ${Math.floor(inSeconds/60/60/24/365)} lat temu`
+    if (inSeconds < 60) {
+      const secs = Math.floor(inSeconds);
+      return `Dodane ${secs > 1 ? secs + " " : ""}${
+        secs > 1 ? (secs < 5 ? "sekundy" : "sekund") : "sekundę"
+      } temu`;
+    } else if (inSeconds >= 60 && inSeconds < 60 * 60) {
+      const minutes = Math.floor(inSeconds / 60);
+      return `Dodane ${minutes} ${
+        minutes > 1 ? (minutes < 5 ? "minuty" : "minut") : "minutę"
+      } temu`;
+    } else if (inSeconds >= 60 * 60 && inSeconds < 60 * 60 * 24) {
+      const hours = Math.floor(inSeconds / 60 / 60);
+      return `Dodane ${hours} ${
+        hours > 1 ? (hours < 5 ? "godziny" : "godzin") : "godzinę"
+      } temu`;
+    } else if (inSeconds >= 60 * 60 * 24 && inSeconds < 60 * 60 * 24 * 7) {
+      const days = Math.floor(inSeconds / 60 / 60 / 24);
+      return `Dodane ${days} ${days > 1 ? "dni" : "dzień"} temu`;
+    } else if (inSeconds >= 60 * 60 * 24 * 7 && inSeconds < 60 * 60 * 24 * 30) {
+      const weeks = Math.floor(inSeconds / 60 / 60 / 24 / 7);
+      return `Dodane ${weeks} ${
+        weeks > 1 ? (weeks < 5 ? "tygodnie" : "tygodni") : "tydzień"
+      } temu`;
+    } else if (
+      inSeconds >= 60 * 60 * 24 * 30 &&
+      inSeconds < 60 * 60 * 24 * 365
+    ) {
+      const months = Math.floor(inSeconds / 60 / 60 / 24 / 30);
+      return `Dodane ${months} ${
+        months > 1 ? (months < 5 ? "miesiące" : "miesięcy") : "miesiąc"
+      } temu`;
+    } else if (inSeconds >= 60 * 60 * 24 * 365) {
+      const years = Math.floor(inSeconds / 60 / 60 / 24 / 365);
+      return `Dodane ${years} ${
+        years > 1 ? (years < 5 ? "lata" : "lat") : "rok"
+      } temu`;
     }
-  }
-
+  };
 
   return (
     <div className="posts">
@@ -93,8 +114,10 @@ const Index = () => {
             {post.comments.map((comment) => (
               <div className="comment" key={comment.id}>
                 <span>
-                  <Link to={`/users/${comment.userId}`}>{comment.userName}</Link>
-                  <br/>
+                  <Link to={`/users/${comment.userId}`}>
+                    {comment.userName}
+                  </Link>
+                  <br />
                   {comment.text}
                 </span>
                 <div className="likes">
