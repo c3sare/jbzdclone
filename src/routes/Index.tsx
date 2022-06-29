02766c275cc701456e84d355adbe9fe942c58c5a
C3sare <marcinm222@gmail.com>
2022-06-29 15:47:54 +0200

    posts avatars added

--- src/routes/Index.tsx
+++ src/routes/Index.tsx
@@ -1,4 +1,5 @@
 import "../styles/Posts.css";
+import avatar from '../images/avatars/default.jpg';
 
 import { Link } from "react-router-dom";
 
@@ -6,6 +7,7 @@ interface Post {
   id: number;
   userId: number;
   userName: string;
+  category: string;
   title: string;
   addDate: string;
   img: string;
@@ -31,6 +33,7 @@ const posts: Post[] = [
     id: 1,
     userId: 1,
     userName: "Anonimowy",
+    category: "Humor",
     title: "tytuł",
     addDate:
       "Fri Jun 24 2022 15:12:00 GMT+0200 (czas środkowoeuropejski letni)",
@@ -66,40 +69,30 @@ const Index = () => {
 
     if (inSeconds < 60) {
       const secs = Math.floor(inSeconds);
-      return `Dodane ${secs > 1 ? secs + " " : ""}${
-        secs > 1 ? (secs < 5 ? "sekundy" : "sekund") : "sekundę"
-      } temu`;
+      return `${secs} sek.`;
     } else if (inSeconds >= 60 && inSeconds < 60 * 60) {
       const minutes = Math.floor(inSeconds / 60);
-      return `Dodane ${minutes} ${
-        minutes > 1 ? (minutes < 5 ? "minuty" : "minut") : "minutę"
-      } temu`;
+      return `${minutes} min.`;
     } else if (inSeconds >= 60 * 60 && inSeconds < 60 * 60 * 24) {
       const hours = Math.floor(inSeconds / 60 / 60);
-      return `Dodane ${hours} ${
-        hours > 1 ? (hours < 5 ? "godziny" : "godzin") : "godzinę"
-      } temu`;
+      return `${hours} godz.`;
     } else if (inSeconds >= 60 * 60 * 24 && inSeconds < 60 * 60 * 24 * 7) {
       const days = Math.floor(inSeconds / 60 / 60 / 24);
-      return `Dodane ${days} ${days > 1 ? "dni" : "dzień"} temu`;
+      return `${days} ${days > 1 ? "dni" : "dzień"}`;
     } else if (inSeconds >= 60 * 60 * 24 * 7 && inSeconds < 60 * 60 * 24 * 30) {
       const weeks = Math.floor(inSeconds / 60 / 60 / 24 / 7);
-      return `Dodane ${weeks} ${
-        weeks > 1 ? (weeks < 5 ? "tygodnie" : "tygodni") : "tydzień"
-      } temu`;
+      return `${weeks} tyg.`;
     } else if (
       inSeconds >= 60 * 60 * 24 * 30 &&
       inSeconds < 60 * 60 * 24 * 365
     ) {
       const months = Math.floor(inSeconds / 60 / 60 / 24 / 30);
-      return `Dodane ${months} ${
-        months > 1 ? (months < 5 ? "miesiące" : "miesięcy") : "miesiąc"
-      } temu`;
+      return `${months} mies.`;
     } else if (inSeconds >= 60 * 60 * 24 * 365) {
       const years = Math.floor(inSeconds / 60 / 60 / 24 / 365);
-      return `Dodane ${years} ${
+      return `${years} ${
         years > 1 ? (years < 5 ? "lata" : "lat") : "rok"
-      } temu`;
+      }`;
     }
   };
 
@@ -107,38 +100,52 @@ const Index = () => {
     <div className="posts">
       {posts.map((post) => (
         <div className="post" key={post.id}>
-          <h2>{post.title}</h2>
-          <p>{alongAgo(post.addDate)}</p>
-          <img src={post.img} alt={post.title} />
-          <div className="comments">
-            {post.comments.map((comment) => (
-              <div className="comment" key={comment.id}>
-                <span>
-                  <Link to={`/users/${comment.userId}`}>
-                    {comment.userName}
-                  </Link>
-                  <br />
-                  {comment.text}
-                </span>
-                <div className="likes">
-                  <button>+</button>
-                  <span>{comment.likes}</span>
-                  <button>-</button>
-                </div>
-                <div className="subComments">
-                  {comment.subcomments.map((subcomment) => (
-                    <div className="subComment" key={subcomment.id}>
-                      <p>{subcomment.text}</p>
-                      <div className="subLikes">
-                        <button>+</button>
-                        <span>{comment.likes}</span>
-                        <button>-</button>
+          <div className="avatar">
+            <Link to={`/user/${post.userId}/${encodeURI(post.userName)}`}>
+              <img src={avatar} alt="Avatar"/>
+            </Link>
+          </div>
+          <div className="contentPost">
+            <h2>{post.title}</h2>
+            <p>
+              {alongAgo(post.addDate)}
+              {" "}
+              <Link to={`/kategoria/${post.category.toLowerCase()}`} style={{color: '#bd3c3c'}}>{post.category}</Link>
+            </p>
+            <img src={post.img} alt={post.title} />
+            <div className="comments">
+              {post.comments.map((comment) => (
+                <div className="comment" key={comment.id}>
+                  <span>
+                    <Link to={`/users/${comment.userId}`}>
+                      {comment.userName}
+                    </Link>
+                    <br />
+                    {comment.text}
+                  </span>
+                  <div className="likes">
+                    <button>+</button>
+                    <span>{comment.likes}</span>
+                    <button>-</button>
+                  </div>
+                  <div className="subComments">
+                    {comment.subcomments.map((subcomment) => (
+                      <div className="subComment" key={subcomment.id}>
+                        <p>{subcomment.text}</p>
+                        <div className="subLikes">
+                          <button>+</button>
+                          <span>{comment.likes}</span>
+                          <button>-</button>
+                        </div>
                       </div>
-                    </div>
-                  ))}
+                    ))}
+                  </div>
                 </div>
-              </div>
-            ))}
+              ))}
+            </div>
+            <div>
+
+            </div>
           </div>
         </div>
       ))}
