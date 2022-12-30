df56b59082ac88ee4eb446d2c72c2ee442999096
c3sare <marcinm222@gmail.com>
2022-12-31 00:07:15 +0100

    jbzd add mem form update

--- src/components/AddPost.tsx
+++ src/components/AddPost.tsx
@@ -1,7 +1,223 @@
 import "../styles/AddPost.css";
+import { FaRegImage, FaVideo, FaYoutube, FaTrashAlt } from "react-icons/fa";
+import { IoDocumentText } from "react-icons/io5";
+import { categories } from "../data/categories";
+import { useState } from "react";
+import { IoClose } from "react-icons/io5";
+import { TiUpload } from "react-icons/ti";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+
+const ImageContainer = () => {
+  return (
+    <div className="imageContainer">
+      <span>
+        <TiUpload />
+      </span>
+      <span>Przeciągnij tu plik</span>
+      <span>lub</span>
+      <button>Przeglądaj</button>
+    </div>
+  );
+};
+
+const TextContainer = () => {
+  const [state, setState] = useState("");
+  return <ReactQuill theme="snow" value={state} onChange={setState} />;
+};
 
 const AddPost = () => {
-  return <div className="addPostContainer"></div>;
+  const [currentCategory, setCurrentCategory] = useState("");
+  const [tags, setTags] = useState<string[]>([]);
+  const [currentTag, setCurrentTag] = useState<string>("");
+  const [showLinking, setShowLinking] = useState<boolean>(false);
+  const [memContainers, setMemContainers] = useState<JSX.Element[]>([]);
+
+  const handleTagname = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.value.indexOf(",") < 0) setCurrentTag(e.target.value);
+  };
+
+  const handleAddTagKeys = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "," || e.key === "Tab") {
+      e.preventDefault();
+      if (tags.length >= 10 || tags.indexOf(currentTag) >= 0) return;
+      if (currentTag === "") return null;
+      setTags([...tags, currentTag]);
+      setCurrentTag("");
+    }
+  };
+
+  const handleToggleLinking = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    setShowLinking(!showLinking);
+  };
+
+  const handleDeleteTag = (
+    e: React.MouseEvent<HTMLButtonElement>,
+    i: number
+  ) => {
+    e.preventDefault();
+    if (tags.indexOf(currentTag) >= 0) return;
+    setTags((prevState) => {
+      const newState = [...prevState].filter((_item, index) => i !== index);
+      return newState;
+    });
+  };
+
+  const handleActiveCategory = (
+    e: React.MouseEvent<HTMLButtonElement>,
+    i: number,
+    prefix = ""
+  ) => {
+    e.preventDefault();
+    setCurrentCategory(prefix + String(i));
+  };
+
+  const handleClearCategory = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    setCurrentCategory("");
+  };
+
+  const handleAddImage = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    setMemContainers((prev) => {
+      const newState = [...prev, <ImageContainer />];
+
+      return newState;
+    });
+  };
+
+  const handleAddText = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    setMemContainers((prev) => {
+      const newState = [...prev, <TextContainer />];
+
+      return newState;
+    });
+  };
+
+  return (
+    <div className="addPostContainer">
+      <form>
+        <div>
+          <h3>Wpisz tytuł</h3>
+          <input placeholder="Wpisz tytuł" type="text" />
+        </div>
+        {memContainers.map((item, i) => (
+          <div className="memElement">
+            {item}
+            <button
+              onClick={(e) => {
+                e.preventDefault();
+                setMemContainers((prev) => {
+                  const newState = [...prev];
+
+                  return newState.filter((_el, index) => index !== i);
+                });
+              }}
+            >
+              <FaTrashAlt /> Usuń element
+            </button>
+          </div>
+        ))}
+        <div>
+          <h3>Co chcesz dodać?</h3>
+          <div className="contentType">
+            <button onClick={handleAddImage}>
+              <FaRegImage />
+              <span>Obrazek/Gif</span>
+            </button>
+            <button onClick={handleAddText}>
+              <IoDocumentText />
+              <span>Tekst</span>
+            </button>
+            <button>
+              <FaVideo />
+              <span>Video MP4</span>
+            </button>
+            <button>
+              <FaYoutube />
+              <span>Youtube</span>
+            </button>
+          </div>
+        </div>
+        <div>
+          <h3>Dodaj tagi</h3>
+          <input
+            type="text"
+            placeholder="Wpisz tagi ..."
+            value={currentTag}
+            onChange={handleTagname}
+            onKeyDown={handleAddTagKeys}
+          />
+          <div className="tags">
+            {tags.map((item, i) => (
+              <span className="tagItem">
+                <span className="title">#{item}</span>
+                <button onClick={(e) => handleDeleteTag(e, i)}>
+                  <IoClose />
+                </button>
+              </span>
+            ))}
+          </div>
+          <span className="info">
+            Aby dodać kolejny tag należy dodać przecinek albo nacisnąć TAB.
+          </span>
+        </div>
+        <div className="categories">
+          <h3>
+            Gdzie chcesz dodać treść?{" "}
+            <span className="optional">{"(opcjonalnie)"}</span>
+            <div>
+              {categories[0].map((item, i) => (
+                <button
+                  className={currentCategory === String(i) ? "active" : ""}
+                  onClick={(e) => handleActiveCategory(e, i)}
+                >
+                  {item.name}
+                </button>
+              ))}
+              {Number(currentCategory) < 100 && currentCategory !== "" && (
+                <button onClick={handleClearCategory} className="emptyButton">
+                  wyczyść
+                </button>
+              )}
+            </div>
+            <div>
+              <span style={{ fontSize: "12px", color: "#de2127" }}>nsfw:</span>
+              <div>
+                {categories[1].map((item, i) => (
+                  <button
+                    className={
+                      currentCategory === "10" + String(i) ? "active" : ""
+                    }
+                    onClick={(e) => handleActiveCategory(e, i, "10")}
+                  >
+                    {item.name}
+                  </button>
+                ))}
+                {Number(currentCategory) >= 100 && currentCategory !== "" && (
+                  <button onClick={handleClearCategory} className="emptyButton">
+                    wyczyść
+                  </button>
+                )}
+              </div>
+            </div>
+            <div className="linking">
+              <button onClick={handleToggleLinking}>
+                {!showLinking ? "Pokaż linkowanie" : "Schowaj linkowanie"}
+              </button>
+              {showLinking && <input placeholder="Wpisz link" type="url" />}
+            </div>
+          </h3>
+        </div>
+        <div className="addCancelButtons">
+          <button className="submit">Dodaj</button>
+          <button>Anuluj</button>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default AddPost;
--- src/routes/obr/Index.tsx
+++ src/routes/obr/Index.tsx
@@ -5,68 +5,78 @@ import posts from "../../data/posts";
 import { useEffect } from "react";
 
 const Index = () => {
-    const {id, url} = useParams();
-    let navigate = useNavigate();
-    console.log(encodeURI(String(url).toLowerCase()));
-    console.log(encodeURI(String(posts.find(item => item.id === Number(id))?.title.toLowerCase())))
-
-    const post = posts.find(item => item.id === Number(id) && encodeURI(String(url).toLowerCase()) === encodeURI(item.title.toLowerCase()));
-    useEffect(() => {
-        if(!post) navigate("/404");
-        else document.title = "Jbzd.com.pl - "+post.title;
-    }, [])
-
-    return (
-        <div className="post">
-          <div className="avatar">
-            <Link to={`/user/${post?.userId}/${encodeURI(String(post?.userName))}`}>
-              <img src={avatar} alt="Avatar"/>
-            </Link>
-          </div>
-          <div className="contentPost">
-            <h2>{post?.title}</h2>
-            <p>
-              {alongAgo(String(post?.addDate))}
-              {" "}
-              <Link to={`/kategoria/${post?.category.toLowerCase()}`} style={{color: '#bd3c3c'}}>{post?.category}</Link>
-            </p>
-            <img src={post?.img} alt={post?.title} />
-            <div className="comments">
-              {post?.comments.map((comment) => (
-                <div className="comment" key={comment.id}>
-                  <span>
-                    <Link to={`/users/${comment.userId}`}>
-                      {comment.userName}
-                    </Link>
-                    <br />
-                    {comment.text}
-                  </span>
-                  <div className="likes">
-                    <button>+</button>
-                    <span>{comment.likes}</span>
-                    <button>-</button>
-                  </div>
-                  <div className="subComments">
-                    {comment.subcomments.map((subcomment) => (
-                      <div className="subComment" key={subcomment.id}>
-                        <p>{subcomment.text}</p>
-                        <div className="subLikes">
-                          <button>+</button>
-                          <span>{comment.likes}</span>
-                          <button>-</button>
-                        </div>
-                      </div>
-                    ))}
-                  </div>
-                </div>
-              ))}
-            </div>
-            <div>
-
-            </div>
-          </div>
-        </div>
+  const { id, url } = useParams();
+  let navigate = useNavigate();
+  console.log(encodeURI(String(url).toLowerCase()));
+  console.log(
+    encodeURI(
+      String(posts.find((item) => item.id === Number(id))?.title.toLowerCase())
     )
-}
+  );
+
+  const post = posts.find(
+    (item) =>
+      item.id === Number(id) &&
+      encodeURI(String(url).toLowerCase()) ===
+        encodeURI(item.title.toLowerCase())
+  );
+  useEffect(() => {
+    if (!post) navigate("/404");
+    else document.title = "Jbzd.com.pl - " + post.title;
+    // eslint-disable-next-line
+  }, []);
+
+  return (
+    <div className="post">
+      <div className="avatar">
+        <Link to={`/user/${post?.userId}/${encodeURI(String(post?.userName))}`}>
+          <img src={avatar} alt="Avatar" />
+        </Link>
+      </div>
+      <div className="contentPost">
+        <h2>{post?.title}</h2>
+        <p>
+          {alongAgo(String(post?.addDate))}{" "}
+          <Link
+            to={`/kategoria/${post?.category.toLowerCase()}`}
+            style={{ color: "#bd3c3c" }}
+          >
+            {post?.category}
+          </Link>
+        </p>
+        <img src={post?.img} alt={post?.title} />
+        <div className="comments">
+          {post?.comments.map((comment) => (
+            <div className="comment" key={comment.id}>
+              <span>
+                <Link to={`/users/${comment.userId}`}>{comment.userName}</Link>
+                <br />
+                {comment.text}
+              </span>
+              <div className="likes">
+                <button>+</button>
+                <span>{comment.likes}</span>
+                <button>-</button>
+              </div>
+              <div className="subComments">
+                {comment.subcomments.map((subcomment) => (
+                  <div className="subComment" key={subcomment.id}>
+                    <p>{subcomment.text}</p>
+                    <div className="subLikes">
+                      <button>+</button>
+                      <span>{comment.likes}</span>
+                      <button>-</button>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+          ))}
+        </div>
+        <div></div>
+      </div>
+    </div>
+  );
+};
 
 export default Index;
